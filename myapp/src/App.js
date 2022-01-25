a5a4c75bbb252ac4d95cf83b4dd1bd7bb52cadb5
tokagi7428 <79756474+tokagi7428@users.noreply.github.com>
2022-01-25 13:47:50 +0700

    menu

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Menu from "./component/Menu";
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/component/Menu.js
@@ -0,0 +1,17 @@
+import "./css/Menu.css";
+function Menu() {
+  return (
+    <div className="menu-bar">
+      <div>
+        <h1>Menu</h1>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
