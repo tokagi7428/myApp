7e50d0f0bc2ac5e5bcd50ed98b9019ed008a4183
tokagi7428 <79756474+tokagi7428@users.noreply.github.com>
2022-01-25 19:46:09 +0700

    portfolio

--- myapp/src/component/Contact.js
+++ myapp/src/component/Contact.js
@@ -5,6 +5,7 @@ function Contact() {
       <h1>My contact</h1>
       <a href="#" class="fa fa-facebook icon"></a>
       <a href="#" class="fa fa-twitter icon"></a>
+      <a href="https://github.com/tokagi7428" class="fa fa-github icon"></a>
       <p>Tel : 095-xxx-xxxx</p>
     </footer>
   );
