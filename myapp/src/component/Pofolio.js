5b085a00549a8344c0fa6e47c4b9f110f59260f4
tokagi7428 <79756474+tokagi7428@users.noreply.github.com>
2022-01-30 15:08:26 +0700

    modified1

--- myapp/src/component/Pofolio.js
+++ myapp/src/component/Pofolio.js
@@ -7,15 +7,21 @@ function Pofolio() {
         <h1>My self</h1>
         <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/p206x206/135150268_856678565165692_2252528332421035118_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=aee45a&_nc_eui2=AeGtsnFIWFwZ_hKG9oo6JDVDrbQ_RDkxuB6ttD9EOTG4Hjrv54zVN34dDUYx9lddVVLxXGqeMw7TfD7gePf8GbsZ&_nc_ohc=O3Ggry96KakAX8-B8wi&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVITQlMb7R9uIZigIKXvTNblrvhrL3bpxE27rI6B3CjpzA&oe=62157CEF"></img>
         <div className="block">
+          <div className="name">
+            <h3>Name</h3>
+            <p>My name's Tanatip Tangjitjaroen</p>
+            <p>NickName's Guide</p>
+            <p>Hobby: เล่นเกมส์ ฟังเพลง ดูหนัง หาความรู้ใหม่ๆ</p>
+          </div>
           <div className="education">
             <h3>My Education</h3>
             <p>School : Suwannaramwittayakom School</p>
             <p>University : Ramkhamhaeng University</p>
             <p>Branch : Com-Sci degree 3</p>
           </div>
-          <div>
+          <div className="skill">
             <h3>My skills</h3>
-            <p>C++ Oop React Java</p>
+            <p>C++ Oop Java React (basic)</p>
           </div>
         </div>
       </div>
