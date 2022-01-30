7ef663f5de0afc981a5163ffffdedd967e75eb6a
tokagi7428 <79756474+tokagi7428@users.noreply.github.com>
2022-01-30 15:12:10 +0700

    Update Pofolio.js

--- myapp/src/component/Pofolio.js
+++ myapp/src/component/Pofolio.js
@@ -4,7 +4,7 @@ function Pofolio() {
   return (
     <div id="portfolio">
       <div className="box">
-        <h1>My self</h1>
+        <h1>Pofolio</h1>
         <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/p206x206/135150268_856678565165692_2252528332421035118_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=aee45a&_nc_eui2=AeGtsnFIWFwZ_hKG9oo6JDVDrbQ_RDkxuB6ttD9EOTG4Hjrv54zVN34dDUYx9lddVVLxXGqeMw7TfD7gePf8GbsZ&_nc_ohc=O3Ggry96KakAX8-B8wi&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVITQlMb7R9uIZigIKXvTNblrvhrL3bpxE27rI6B3CjpzA&oe=62157CEF"></img>
         <div className="block">
           <div className="name">
